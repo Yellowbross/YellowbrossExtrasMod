76e1016db3a3d30be9546b0c324ec5b32cd6c295
Yellowbross <yellowbrossanimations@gmail.com>
2026-06-02 09:30:33 -0400

    Reenabled all of Defender's P1 attacks

--- src/main/java/com/yellowbrossproductions/yellowbrossextras/entities/defender/DefenderEntity.java
+++ src/main/java/com/yellowbrossproductions/yellowbrossextras/entities/defender/DefenderEntity.java
@@ -203,14 +203,14 @@ public class DefenderEntity extends YExtrasMob implements YextrasEntity, IsDefen
         this.goalSelector.addGoal(0, new RatatatabowGoal(this));
 
         this.goalSelector.addGoal(0, new ExcaliburGoal(this));
-        // this.goalSelector.addGoal(0, new ClawsGoal(this));
-        // this.goalSelector.addGoal(0, new ChainsawGoal(this));
-        // this.goalSelector.addGoal(0, new ShurikensGoal(this));
-        // this.goalSelector.addGoal(0, new SpikesGoal(this));
-        // this.goalSelector.addGoal(0, new BoomerangGoal(this));
+        this.goalSelector.addGoal(0, new ClawsGoal(this));
+        this.goalSelector.addGoal(0, new ChainsawGoal(this));
+        this.goalSelector.addGoal(0, new ShurikensGoal(this));
+        this.goalSelector.addGoal(0, new SpikesGoal(this));
+        this.goalSelector.addGoal(0, new BoomerangGoal(this));
         this.goalSelector.addGoal(0, new AxesGoal(this));
-        // this.goalSelector.addGoal(0, new SwordGoal(this));
-        // this.goalSelector.addGoal(0, new SawsGoal(this));
+        this.goalSelector.addGoal(0, new SwordGoal(this));
+        this.goalSelector.addGoal(0, new SawsGoal(this));
 
         this.goalSelector.addGoal(0, new DefeatedGoal());
         this.goalSelector.addGoal(0, new JumpAwayGoal(this));
