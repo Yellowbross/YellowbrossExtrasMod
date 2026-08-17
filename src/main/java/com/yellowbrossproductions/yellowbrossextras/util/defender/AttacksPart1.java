ec6f48911bd92ade7d365de59022a18531002be3
Yellowbross <yellowbrossanimations@gmail.com>
2026-08-17 07:38:21 -0400

    Final tweaks

--- src/main/java/com/yellowbrossproductions/yellowbrossextras/util/defender/AttacksPart1.java
+++ src/main/java/com/yellowbrossproductions/yellowbrossextras/util/defender/AttacksPart1.java
@@ -1194,7 +1194,7 @@ public class AttacksPart1 {
                 }
                 if (defender.deathAttackTicks == 600) {
                     Vec3 landingPosition = target != null ? target.position().add(target.getLookAngle().scale(16.0D).multiply(1, 0, 1)) : defender.position();
-                    BlockPos.MutableBlockPos yPosition = new BlockPos.MutableBlockPos(landingPosition.x, landingPosition.y, landingPosition.z);
+                    BlockPos.MutableBlockPos yPosition = new BlockPos.MutableBlockPos(landingPosition.x, landingPosition.y + defender.level.getMaxBuildHeight(), landingPosition.z);
                     while (yPosition.getY() > defender.level.getMinBuildHeight() && !defender.level.getBlockState(yPosition).getMaterial().blocksMotion()) {
                         yPosition.move(Direction.DOWN);
                     }
